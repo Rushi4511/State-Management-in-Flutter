fbdec1659f2cd886d7a3eae6bd1a47d216c4acce
Rushi4511 <rushikarle45@gmail.com>
2025-01-29 12:48:23 +0530

    Change Notifier provider Demo App:Using Employee Example

--- change_notifier_provider_demo/lib/main.dart
+++ change_notifier_provider_demo/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +13,88 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return ChangeNotifierProvider(
+      create: (context) {
+        return Employee(
+          empName: "Rushikesh",
+          empId: 1,
+          empSal: 10000.00,
+        );
+      },
+      child: const MaterialApp(home: EmployeeView()),
+    );
+  }
+}
+
+class EmployeeView extends StatefulWidget {
+  const EmployeeView({super.key});
+
+  @override
+  State<EmployeeView> createState() => _EmployeeViewState();
+}
+
+class _EmployeeViewState extends State<EmployeeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("ChangeNotifier Provider",
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            )),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(25),
+        child: Column(
+          children: [
+            Text("Employee ID :${Provider.of<Employee>(context).empId}",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                )),
+            const SizedBox(
+              height: 20,
+            ),
+            Text("Employee Name : ${Provider.of<Employee>(context).empName}",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                )),
+            const SizedBox(
+              height: 20,
+            ),
+            Text("Employee Salary :${Provider.of<Employee>(context).empSal} ",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                )),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Provider.of<Employee>(context, listen: false).changeData(25000.00);
+        },
+      ),
     );
+  }
+}
+
+class Employee extends ChangeNotifier {
+  String empName;
+  int empId;
+  double empSal;
+
+  Employee({
+    required this.empId,
+    required this.empName,
+    required this.empSal,
+  });
+
+  void changeData(double empSal) {
+    this.empSal = empSal;
+    notifyListeners();
   }
 }
