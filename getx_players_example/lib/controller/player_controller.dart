a6184c5c110e6696067969369021f7223a5d4564
Rushi4511 <rushikarle45@gmail.com>
2025-02-21 23:35:43 +0530

    Controller file

--- getx_players_example/lib/controller/player_controller.dart
+++ getx_players_example/lib/controller/player_controller.dart
@@ -1,3 +1,10 @@
-class PlayerController extends GetxController{
-  
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:getx_players_example/model/Player.dart';
+
+class PlayerController extends GetxController {
+  Player playerObj =
+      Player(playerName: "Rohit", playerRole: "Batsman", team: "India");
+
+
+    
 }
