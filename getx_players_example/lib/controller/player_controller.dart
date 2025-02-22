1e6cf3fb0475f11893cc860fdbc2700c0f24c24c
Rushi4511 <rushikarle45@gmail.com>
2025-02-22 23:49:27 +0530

    Update method

--- getx_players_example/lib/controller/player_controller.dart
+++ getx_players_example/lib/controller/player_controller.dart
@@ -1,10 +1,16 @@
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:getx_players_example/model/Player.dart';
 
 class PlayerController extends GetxController {
-  Player playerObj =
-      Player(playerName: "Rohit", playerRole: "Batsman", team: "India");
+  Rx<Player> playerObj =
+      Player(playerName: "Rohit", playerRole: "Batsman", team: "India").obs;
 
 
-    
+    void changeData(String playerName, String playerRole) {
+    playerObj.update((obj) {
+      obj!.playerName = playerName;
+      obj.playerRole = playerRole;
+    });
+  }
 }
